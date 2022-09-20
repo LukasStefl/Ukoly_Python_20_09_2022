0c1431e65d248ebe031278f1e93dc67ad4e4d122
LukasStefl <lukas.stefl@student.ossp.cz>
2022-09-20 15:15:06 +0200

    commit

--- Druhy_ukol.py
+++ Druhy_ukol.py
@@ -2,7 +2,7 @@ print("Napiste dve cisla:\n")
 prvni_cislo = int(input());
 druhe_cislo = int(input());
 
-if (prvni_cislo * druhe_cislo) < 1000:
+if (prvni_cislo * druhe_cislo) <= 1000:
     vysledek = prvni_cislo * druhe_cislo;
     print(vysledek);
 elif (prvni_cislo * druhe_cislo) > 1000:
